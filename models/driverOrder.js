dc9bd805a12e0c05508ea520e025c4661c19a839
Andrey Sholinka <sholinka2111@gmail.com>
2021-03-15 11:42:33 +0700

    15/03/21 11.42 add driverOrder.js

---
+++ models/driverOrder.js
@@ -0,0 +1,100 @@
+const debug = require('debug')('app:model:user');
+const pool = require('../libs/db');
+const {
+    hash
+} = require('bcrypt');
+const jsotp = require('jsotp');
+const totp = jsotp.TOTP('BASE32ENCODEDSECRET');
+
+const schema = '"public"';
+const table = '"users"'
+const dbTable = schema + '.' + table;
+const jfoodviews = 'orders.jfood';
+const orderstb = 'orders.orders';
+
+class DriverOrderModel {
+
+    async incomingorder(kode) {
+
+        try {
+            let res = await pool.query(`SELECT * FROM ${jfoodviews} WHERE "kode" = '${kode}'`);
+            debug('incomingorder %o', res);
+            if (res.rowCount <= 0) {
+                console.log("Kode Tidak Tersedia");
+                return {
+                    "status": "404",
+                    "errors": "Kode " + kode + " tidak terdaftar"
+                }
+            } else {
+                return res.rows[0];
+            }
+        } catch (ex) {
+            console.log('Error : ' + ex);
+        };
+    }
+
+    async acceptorder(kode, id) {
+        try {
+            pool.query("call orders.order_jfood_accepted_by_driver('" + kode + "', _driver_id => " + id + ")", (error, results) => {
+                if (error) {
+                    return console.error(error.message);
+                }
+            });
+        } catch (ex) {
+            console.log('Error : ' + ex);
+        };
+    }
+
+    async dataorder(kode) {
+
+        try {
+            let driver = await pool.query(`SELECT latitude_location_driver_start, longitude_location_driver_start FROM ${orderstb} WHERE "kode" = '${kode}'`);
+            let order = await pool.query(`SELECT jarak, latitude_restaurant, longitude_restaurant, customer_name, phone, landmark_restaurant, address_restaurant, kode, jumlah_menu, jumlah_pesanan, menu_name, menu_quantity, menu_catatan, menu_price_merchant, menu_price_customer FROM ${jfoodviews} WHERE "kode" = '${kode}'`);
+            debug('dataorder %o', order);
+            if (order.rowCount <= 0) {
+                console.log("Kode Tidak Tersedia");
+                return {
+                    "status": "404",
+                    "errors": "Kode " + kode + " tidak terdaftar"
+                }
+            } else {
+                return {
+                    driver: driver.rows[0],
+                    order: order.rows,
+                }
+            }
+        } catch (ex) {
+            console.log('Error : ' + ex);
+        };
+    }
+
+    async rejectorder(kode, id) {
+        try {
+            pool.query("call orders.order_jfood_rejected_by_driver('" + kode + "', _driver_id => " + id + ")", (error, results) => {
+                if (error) {
+                    return console.error(error.message);
+                }
+            });
+        } catch (ex) {
+            console.log('Error : ' + ex);
+        };
+    }
+
+    async updatedatadriver(id, lat, long, token) {
+
+        try {
+            let res = await pool.query("UPDATE " + dbTable + " SET (latitude_position, longitude_position, token_notification) = ($1, $2, $3) WHERE id = " + id + " RETURNING latitude_position, longitude_position, token_notification", [lat, long, token]);
+            debug('updatedatadriver %o', res);
+            if (res.rowCount <= 0) {
+                throw 'Edit fail';
+            } else {
+                return res.rows[0]
+            }
+        } catch (ex) {
+            console.log('Enek seng salah iki ' + ex)
+        };
+    }
+
+}
+
+module.exports = new DriverOrderModel();
