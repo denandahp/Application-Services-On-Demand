be974b81544dbf9b4840d23b5a0475af47ff1aa7
Denanda Hendra <denandahp@gmail.com>
2021-04-19 12:17:19 +0700

    19/04/2021 12:17

--- routes/customerNotifjride.js
+++ routes/customerNotifjride.js
@@ -1,6 +1,6 @@
 const Router = require('express').Router();
 const customerNotifjride = require('../controllers/customerNotifjride.js');
 
-Router.post('/orderjride/customertodriver', notification.orderjride_customertodriver)
+Router.post('/orderjride/customertodriver', customerNotifjride.orderjride_customertodriver)
       
 module.exports = Router;
