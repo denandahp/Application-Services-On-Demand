2c51cac29e95589e3c57b2e2e7a3f612cb856f3e
Andrey Sholinka <sholinka2111@gmail.com>
2021-04-24 12:04:42 +0700

    24/04/21 12.04 - topup midtrans

--- controllers/paymentUtils.js
+++ controllers/paymentUtils.js
@@ -1,8 +1,34 @@
 const axios = require('axios');
+const pool = require('../libs/db');
+const topuptb = 'driver.topup';
 
 class PaymentIntegrationController {
     async generateSnap(req, res, next) {
-        // req.body.transaction_details.order_id = "order-csb-" + Math.round(new Date().getTime() / 1000)
+
+        var d = new Date(Date.now());
+        var dd = d.getDate();
+        var mm = d.getMonth();
+        var y = d.getFullYear();
+        var hour = d.getHours();
+        var minute = d.getMinutes();
+        var second = d.getSeconds();
+        var FormattedDate = y + ':' + mm + ':' + dd + ' ' + hour + ':' + minute + ':' + second;
+
+        let id = req.user.data.id;
+        let nd = req.user.data.namadepan;
+        let nb = req.user.data.namabelakang;
+        let email = req.user.data.email;
+        let phone = req.user.data.phone;
+        let nominal = req.body.transaction_details.gross_amount;
+        req.body.transaction_details.order_id = "driver-" + id + "-" + FormattedDate + "-" + nominal;
+        req.body.customer_details.first_name = nd;
+        req.body.customer_details.last_name = nb;
+        req.body.customer_details.email = email;
+        req.body.customer_details.phone = phone;
+        req.body.credit_card.secure = true;
+        let topup_id = req.body.transaction_details.order_id;
+        let value = [topup_id, id, 'PENDING', nominal, d, d]
+
         try {
             const result = await axios({
                 method: "post",
@@ -19,6 +45,9 @@ class PaymentIntegrationController {
                 req.body.transaction_details.token = response.data
                 res.status(200).json(response.data);
             })
+            if (res.status(200)) {
+                await pool.query('INSERT INTO ' + topuptb + ' (topup_id, driver_id, transaction_status, nominal, created_at, updated_at)VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;', value);
+            }
         } catch (error) {
             res.send(error)
         }
---
+++ models/driverPayment.js
@@ -0,0 +1,24 @@
+const debug = require('debug')('app:model:user');
+const pool = require('../libs/db');
+const {
+    hash
+} = require('bcrypt');
+const jsotp = require('jsotp');
+const totp = jsotp.TOTP('BASE32ENCODEDSECRET');
+
+const topuptb = 'driver.topup';
+
+class PaymentActivityModel {
+    async add(data) {
+
+        let value = [data.transaction_status, data.transaction_id, data.signature_key, data.payment_type, data.transaction_time]
+
+        let res = await pool.query('UPDATE ' + topuptb + ' SET (transaction_status, transaction_id, signature_key, payment_type, updated_at) = ($1, $2, $3, $4, $5) WHERE topup_id = ' + data.order_id + ' RETURNING *;', value);
+        debug('add %o', res);
+
+        return res;
+
+    }
+}
+
+module.exports = new PaymentActivityModel();
