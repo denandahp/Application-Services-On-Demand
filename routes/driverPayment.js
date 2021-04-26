a3f91b2323c0dd1612190430761f845a5826f23c
Andrey Sholinka <sholinka2111@gmail.com>
2021-04-26 10:07:10 +0700

    26/04/21 10.06 - update midtrans

--- routes/driverPayment.js
+++ routes/driverPayment.js
@@ -3,6 +3,6 @@ const auth = require('../middleware/auth.js');
 const paymentActivity = require('../controllers/driverPayment.js');
 const paymentIntegration = require('../controllers/paymentUtils.js');
 
-Router.post('/activity/', auth, paymentActivity.add)
+Router.post('/activity/', paymentActivity.add)
     .post('/snap', auth, paymentIntegration.generateSnap)
 module.exports = Router;
