0a28da40b236dd155622cdd5eb3d2ce0b746cf67
Andrey Sholinka <sholinka2111@gmail.com>
2021-04-26 03:46:01 +0000

    Update driverPayment.js

--- models/driverPayment.js
+++ models/driverPayment.js
@@ -11,7 +11,8 @@ const topuptb = 'driver.topup';
 class PaymentActivityModel {
     async add(data) {
 
-        let value = [data.transaction_status, data.transaction_id, data.signature_key, data.payment_type, data.transaction_time]
+        var d = new Date(Date.now());
+        let value = [data.transaction_status, data.transaction_id, data.signature_key, data.payment_type, d]
         console.log(value);
         let res = await pool.query('UPDATE ' + topuptb + ' SET (transaction_status, transaction_id, signature_key, payment_type, updated_at) = ($1, $2, $3, $4, $5) WHERE topup_id = ' + data.order_id + ' RETURNING *;', value);
         debug('add %o', res);
